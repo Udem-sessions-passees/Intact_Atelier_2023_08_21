5563c2df6c07a0689ed27675b75bfe37bc9b2094
William Morissette <william.morissette26@gmail.com>
2023-08-22 11:46:09 -0400

    fix imports

--- core/data/create_df_with_profit.py
+++ core/data/create_df_with_profit.py
@@ -1,6 +1,6 @@
-from Intact_Atelier_2023_08_21.code.data.models import Models
-from Intact_Atelier_2023_08_21.code.data.models_dict_v2 import model_dict
-from Intact_Atelier_2023_08_21.code.data.generate_synthetic_df import generate_synthetic_df
+from core.data.models import Models
+from core.data.models_dict_v2 import model_dict
+from core.data.generate_synthetic_df import generate_synthetic_df
 
 # Create synthetic dataframe
 df = generate_synthetic_df(10, 0)
